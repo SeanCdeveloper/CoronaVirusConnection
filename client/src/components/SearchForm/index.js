614759635e59094799ef492a8b82e5d00c2eeda6
Your Name <you@example.com>
2020-04-19 20:55:05 -0400

    Add.

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 const SearchForm = () => {
     return(
     <div className="Container">
-        <input style={{marginTop: "150px"}}></input>
+        <input style={{marginTop: "150px", height: "40px",width: "50em"}}/>
         <br/>
-        <input></input>
+        <input style={{marginTop: "30px", height: "40px",width: "50em"}}/>
         <br/>
-        <button type="button"></button> 
+        <button style={{width: "10em", height: "4em",marginTop: "4em"}} type="button">Button</button> 
     </div>
     )
 }
