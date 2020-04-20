3c732a04e9ed20d13a1a30553dd94ed070afa342
Your Name <you@example.com>
2020-04-19 20:35:43 -0400

    Added mock input.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import SearchForm from './components/SearchForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>Hello</div>
+        <SearchForm/>
       </div>
     );
   }
---
+++ client/src/components/SearchForm/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchForm = () => {
+    return(
+    <div className="Container">
+        <input style={{marginTop: "150px"}}></input>
+        <br/>
+        <input></input>
+        <br/>
+        <button type="button"></button> 
+    </div>
+    )
+}
+
+export default SearchForm;
