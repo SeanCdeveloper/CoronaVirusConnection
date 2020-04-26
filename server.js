0f659e97de1eb238de67232d79d827df0de13cfe
Your Name <you@example.com>
2020-04-25 23:33:50 -0400

    API Call is displaying on page. It needs styling desperately but is displaying, lol.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/chatdb");
 //     res.redirect('/');
 //   });
 
-// app.get("/news", )
+// app.get("/news")
 
 // Send every request to the React app
 // Define any API routes before this runs
