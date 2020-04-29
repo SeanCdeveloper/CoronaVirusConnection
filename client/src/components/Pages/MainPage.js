2d2f42de5f701deba0d36c855eefe5fb6dee50bc
Your Name <you@example.com>
2020-04-28 22:48:17 -0400

    Going back to previous file.  Remember to 'npm start' in client and 'node index.js' in server.

---
+++ client/src/components/Pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatPage from '../ChatPage/ChatPage';
+
+const MainPage = () => {
+    return(
+    <div>
+        <ChatPage />
+    </div>
+    )
+}
+
+export default MainPage;
