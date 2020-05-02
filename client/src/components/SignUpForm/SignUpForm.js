865a11c7a9ef0595b79723cee5e568f62e7ff9bf
Your Name <you@example.com>
2020-05-02 15:59:14 -0400

    Added front-end code to connect passport to back-end.

--- client/src/components/SignUpForm/SignUpForm.js
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -1,45 +1,65 @@
 import React, { useState } from 'react';
-import { Header, Container, Button, Form } from 'semantic-ui-react'
+import { Container, Header, Button, Form } from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
 // import AppLogo from "../Logo/index";
+// import axios from 'axios';
 
-export default function SignUpForm() {
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+export default function LogInForm() {
+    // const [username, setUserName] = useState('');
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
 
-  const handleFormSubmit = e => {
-    e.preventDefault();  
-    console.log("Username is: " + username + ", and " + "Password is: " + password)
+    // const [formObject, setFormObject] = useState({});
+
+    const handleFormSubmit = e => {
+        e.preventDefault();  
+        // console.log("Username is: " + username + ", and " + "Password is: " + password)
+        var userData = {
+          email, 
+          password
+        }
+        // console.log('++++++',userData);
+        if (!userData.email || !userData.password) {
+          return;
+        }
+        signUpUser(userData.email, userData.password);
+    };
+
+     const signUpUser = () =>  {
+      console.log("------>",email,password);
+      const signInData = {email, password};
+      console.log(signInData);
+      // axios.post('http://localhost:3000/api/login', {signInData})
+    }
+// axios.post('http://localhost:3001/api/signup', {email,password});
+  return (
+    <Container>
+      {/* <AppLogo/> */}
+    <Header as='h1'>Sign-in Page</Header>
+    <Form onSubmit={handleFormSubmit}>
+      <Form.Field>
+        <label>Username</label>
+        <input style={{ marginTop: "125px", height: "40px", width: "50em" }}
+                type="text"
+                name="username"
+                onChange={(event) => setEmail(event.target.value)} />
+      </Form.Field>
+      <Form.Field>
+        <label>Password</label>
+        <input style={{ marginTop: "30px", height: "40px", width: "50em" }}
+                type="password"
+                name="password"
+                onChange={(event) => setPassword(event.target.value)}
+                />
+      </Form.Field>
+      <Link to="/join">
+      <Button type='submit'>Login</Button>
+      </Link>
+      <Link to="/">Need to Sign-Up?</Link>
+    </Form>
+    </Container>
+  )
 };
-
-    return (
-      <Container>
-        {/* <AppLogo/> */}
-        <Header as='h1'>Sign Up Page</Header>
-        <Form onSubmit={handleFormSubmit}>
-          <Form.Field>
-            <label>Username</label>
-            <input style={{ marginTop: "125px", height: "40px", width: "50em" }}
-              type="text"
-              name="username"
-              onChange={(event) => setUserName(event.target.value)} />
-          </Form.Field>
-          <Form.Field>
-            <label>E-mail</label>
-            <input style={{ marginTop: "30px", height: "40px", width: "50em" }}
-              type="password"
-              name="password"
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </Form.Field>
-          <Link to="/">
-          <Button type='submit'>Sign Up</Button>
-          </Link>
-        </Form>
-        <Link to="/">Need To Login, Instead?</Link>
-      </Container>
-    )
-  };
 
   /* <Button/> '/' changed from '/login' to '/' */
 
