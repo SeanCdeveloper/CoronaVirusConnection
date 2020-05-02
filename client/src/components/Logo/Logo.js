b899bd8bf85234124661021014986fa222da875b
tagine <rana@ranafayez.com>
2020-05-02 19:19:51 -0400

    logo added and two news accordion components added for dev team to choose

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Image } from "semantic-ui-react"
-import cclogo from "../../img/cclogo.png"; // Tell webpack this JS file uses this image
+import cclogo from "../../images/cclogo.png"; // Tell webpack this JS file uses this image
 
 const Logo = () => {
    return <Image src= {cclogo} size="small" wrapped/>
