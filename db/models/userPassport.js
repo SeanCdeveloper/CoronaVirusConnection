3daff50a587c901e928959a67ed443adfe7b4ec6
Adrian Romero <romeroaadrian@gmail.com>
2020-05-03 14:48:20 -0400

    passporthalfway

--- db/models/userPassport.js
+++ db/models/userPassport.js
@@ -50,12 +50,12 @@ const userSchema = new Schema({
   date: { type: Date, default: Date.now },
 });
 
-// userSchema.methods.generateHash = function(password) {
-//   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-// };
-// userSchema.methods.validPassword = function(password) {
-//   return bcrypt.compareSync(password, this.password);
-// };
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 // Define hooks for pre-saving
 userSchema.pre('save', function(next) {
